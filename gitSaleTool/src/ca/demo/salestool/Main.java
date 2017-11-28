c8d4428c11f035218605151e8296aa3bad6c0d1d
madhu <madhu@LAPTOP-EFUV92DE>
2017-11-27 19:26:33 -0500

    Initial Checkin of sales tool.

---
+++ gitSaleTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,21 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData data=new SalesData();
+		displayGreeting();
+		data.display();
+
+		System.out.println("---------");
+	}
+	
+	public static void displayGreeting()
+	{
+		System.out.println("Hello happiee sales peeps !!!");
+		System.out.println("This app shows sales data");
+	}
+
+}
